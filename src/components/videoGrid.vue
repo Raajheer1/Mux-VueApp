0848751f6683100b65d950eea9e69618bb203607
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-02-23 02:50:41 -0600

    Columns fix attempt 1

--- src/components/videoGrid.vue
+++ src/components/videoGrid.vue
@@ -1,12 +1,14 @@
 <template>
   <v-container>
     <v-row>
-      <v-col v-for="item in streams" :key="item">
-        <div v-if="item.status == 'active'">
-          <VideoPlayer :options="item" />
-        </div>
-        <div v-else>
-          Idle Stream
+      <v-col v-for="column in columns" :key="column">
+        <div v-for="item in column" :key="item">
+          <div v-if="item.status == 'active'">
+            <VideoPlayer :options="item" />
+          </div>
+          <div v-else>
+            Idle Stream
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -21,13 +23,23 @@ export default {
   data() {
     return {
       stuff: null,
+      cols: 3
     }
   },
   props: {
     streams: Array
   },
   watch: {},
-  computed: {},
+  computed: {
+    columns () {
+      let columns = []
+      let mid = Math.ceil(this.streams.length / this.cols)
+      for (let col = 0; col < this.cols; col++) {
+        columns.push(this.streams.slice(col * mid, col * mid + mid))
+      }
+      return columns
+    }
+  },
   components: { VideoPlayer },
   methods: {
 
