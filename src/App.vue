e11ddaec0a22ada659af7f1b47bd9d1eef1aac03
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-02-22 20:28:56 -0600

    Main Commit

---
+++ src/App.vue
@@ -0,0 +1,158 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar
+        app
+        clipped-right
+        flat
+        height="72"
+    >
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="156">
+        <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        width="300"
+    >
+      <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          color="grey lighten-3"
+          mini-variant
+      >
+        <v-avatar
+            class="d-block text-center mx-auto mt-4"
+            color="grey darken-1"
+            size="36"
+        ></v-avatar>
+
+        <v-divider class="mx-3 my-5"></v-divider>
+
+        <v-avatar
+            v-for="n in 6"
+            :key="n"
+            class="d-block text-center mx-auto mb-9"
+            color="grey lighten-1"
+            size="28"
+        ></v-avatar>
+      </v-navigation-drawer>
+
+      <v-sheet
+          color="grey lighten-5"
+          height="128"
+          width="100%"
+      ></v-sheet>
+<!--      LEFT BAR      -->
+      <v-list
+          class="pl-14"
+          shaped
+      >
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Overview</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Stream Analytics</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>New Stream</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Manage Streams</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+        app
+        clipped
+        right
+    >
+      <v-list>
+        <v-list-item
+            v-for="n in 5"
+            :key="n"
+            link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Stream {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+
+    <v-footer
+        app
+        color="transparent"
+        height="72"
+        inset
+    >
+      <v-text-field
+          background-color="grey lighten-1"
+          dense
+          flat
+          hide-details
+          rounded
+          solo
+      ></v-text-field>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+const Mux = require('@mux/mux-node');
+const { Video } = new Mux("577af3aa-3e59-4a98-8462-07b369ee48cd", "fB0dD77dio5/isG2FAJ8uvVBAeqKHY106t4KrRvr5rJVOdK4iCnijgtLmkkPEUUA5MhvmgkmM3/");
+Video.LiveStreams.list({ limit: 100, page: 2 });
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: null,
+      STREAM: ""
+    }
+  },
+  components: { },
+  methods: {
+
+  }
+
+}
+</script>
