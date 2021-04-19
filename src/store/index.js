df1e91a62343e39052a00bbab67cea4015fcf0a0
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-04-19 08:10:44 -0500

    Added persistant

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
@@ -26,5 +31,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
